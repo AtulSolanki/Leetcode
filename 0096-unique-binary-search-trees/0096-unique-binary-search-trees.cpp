aaad116d1802ec300b068e50c21cd089555d8a88
AtulSolanki <90123651+AtulSolanki@users.noreply.github.com>
2023-10-05 15:55:55 +0530

    Time: 2044 ms (9.2%), Space: 6.2 MB (32.03%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,19 +1,14 @@
 
 class Solution {
 public:
-    
+
     int numTrees(int n) {
-          vector<int>dp(n+1);
-          dp[0]=1 , dp[1] = 1;
-          for(int i=2;i<=n;i++){
-              dp[i] = 0;
-              for(int j=0;j<i;j++){
-                  int left = dp[j];
-                  int right = dp[i-j-1];
-                  dp[i] += left*right;
-              }
-          }
-          return dp[n];
+         
+            if(n <= 1) return 1;
+        int ans = 0;
+        for(int i = 1; i <= n; i++) 
+            ans += numTrees(i-1) * numTrees(n-i);
+        return ans;
     }
 };
 
