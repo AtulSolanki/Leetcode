f3f45ce938e2024cd0d4a78326b6bf273614572b
AtulSolanki <90123651+AtulSolanki@users.noreply.github.com>
2023-10-07 04:19:09 +0530

    Time: 111 ms (92.11%), Space: 75.6 MB (91.03%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,54 +1,20 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        stack<pair<int,int>>st;
-        int n =heights.size();
-        int left[n],right[n];
-        for(int i=0;i<n;i++){
-             
-             while(!st.empty()){
-                 int idx = st.top().first;
-                 int val = st.top().second;
-                 if(val<heights[i]){
-                     left[i]=idx;
-                     break;
-                 }
-                 else st.pop();
+        int n = heights.size();
+        stack<int>st;
+        int maxi = 0;
+         for(int i=0;i<=n;i++){
+             while(!st.empty() && (i==n || heights[st.top()] >= heights[i])){
+                 int h = heights[st.top()];
+                 st.pop();
+                 int width;
+                 if(st.empty())width = i;
+                 else width = i-st.top()-1;
+                 maxi  = max(maxi, h*width);
              }
-             if(st.empty()){
-                 left[i]=-1;
-             }
-             st.push({i,heights[i]});
-        }
-        while(!st.empty())st.pop();
-        for(int i=n-1;i>=0;i--){
-            
-             while(!st.empty()){
-                 int idx = st.top().first;
-                 int val = st.top().second;
-                 if(val<heights[i]){
-                     right[i]=idx;
-                     break;
-                 }
-                 else st.pop();
-             }
-              if(st.empty()){
-                 right[i]=n;
-             }
-             st.push({i,heights[i]});
-        }
-        for(int i=0;i<n;i++){
-            cout << left[i] << " ";
-        }
-        cout << endl;
-         for(int i=0;i<n;i++){
-            cout << right[i] << " ";
-        }
-        cout << endl;
-        int maxi=0;
-        for(int i=0;i<n;i++){
-            maxi = max(maxi,(right[i]-left[i]-1)*heights[i]);
-        }
-        return maxi;
+             st.push(i);
+         }
+         return maxi;
     }
 };
