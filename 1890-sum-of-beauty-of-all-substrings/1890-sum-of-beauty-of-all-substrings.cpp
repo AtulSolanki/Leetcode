8ad4fadc1ee770ec591d2b70412d2adc5afe5f0d
AtulSolanki <90123651+AtulSolanki@users.noreply.github.com>
2023-10-10 00:43:38 +0530

    Time: 20 ms (99.27%), Space: 6.8 MB (90.33%) - LeetHub

--- 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.cpp
+++ 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
-    int beautySum(string s) {
-        int n = s.size();
-        vector<vector<int>>freq(n+1,vector<int>(26,0));
-        long long int ans = 0;
-        freq[1][s[0]-'a'] = 1;
-        for(int i=1;i<n;i++){
-            freq[i+1][s[i]-'a'] = 1+freq[i][s[i]-'a'];
-            for(int k=0;k<26;k++){
-                 if(s[i]-'a' != k )freq[i+1][k] = freq[i][k];
-    
+   int beautySum(string s) {
+    int res = 0;
+    for (auto i = 0; i < s.size(); ++i) {
+        int cnt[26] = {}, max_f = 0, min_f = 0;
+        for (auto j = i; j < s.size(); ++j) {
+            int idx = s[j] - 'a';
+            max_f = max(max_f, ++cnt[idx]);
+            if(min_f > cnt[idx] - 1){
+                 min_f = cnt[idx];
+            }
+            else if (min_f >= cnt[idx] - 1) {
+                min_f = cnt[idx];
+                for (int k = 0; k < 26; ++k)
+                    min_f = min(min_f, cnt[k] == 0 ? INT_MAX : cnt[k]);
             }
             
+            res += max_f - min_f;
         }
-        // for(int i=0;i<=n;i++){
-        //     for(int k=0;k<26;k++)cout << freq[i][k] << " ";
-        //     cout << endl;
-        // }
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                int mini = INT_MAX , maxi = INT_MIN; 
-                for(int k=0;k<26;k++){
-                    maxi = max(maxi,freq[j+1][k]-freq[i][k]);
-                    if(freq[j+1][k]-freq[i][k]>=1)mini = min(mini,freq[j+1][k]-freq[i][k]);
-                }
-                //if(mini)
-                ans += (maxi-mini);
-            }
-        }
-        return ans;
     }
+    return res;
+
+}
 };
