53bbc29e34307dd13294bf5711d131ed9714a0ac
AtulSolanki <90123651+AtulSolanki@users.noreply.github.com>
2023-10-09 22:36:36 +0530

    Time: 0 ms (100%), Space: 8 MB (21.46%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,22 +1,22 @@
-class Solution
-{
-    public:
-        int solve(vector<int> &nums, int i, int j)
-        {
-            if (i > j)
-            {
-                return 0;
-            }
-            int take_i = nums[i] - solve(nums,i+1,j);
-            int take_j = nums[j] - solve(nums, i, j - 1);
-            return max(take_i, take_j);
-        }
-    bool predictTheWinner(vector<int> &nums)
-    {
-        int n = nums.size(), total = 0;
-        for (auto x: nums) total += x;
-        int sum = solve(nums, 0, n - 1);
-        if (sum >= 0) return true;
+class Solution {
+public:
+    int dp[21][21];
+    int solve(vector<int> &nums, int i , int j){
+       if(i>j){
+          return 0;
+       } 
+       if(dp[i][j]!=-1)return dp[i][j];
+       int take_i = nums[i] + min(solve(nums,i+2,j),solve(nums,i+1,j-1));
+       int take_j = nums[j] + min(solve(nums,i+1,j-1),solve(nums,i,j-2));
+       return dp[i][j]  = max(take_i,take_j);
+    }
+    bool predictTheWinner(vector<int>& nums) {
+        int n = nums.size(),total = 0 ;
+        memset(dp,-1,sizeof(dp));
+        for(auto x: nums)total += x;
+        int sum =  solve(nums,0,n-1);
+        //cout << total << " " << sum << endl;
+        if(sum>=total-sum)return true;
         return false;
     }
 };
