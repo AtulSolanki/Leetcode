249f5224538dcb5cc245e6f1319a5fea4d043a24
AtulSolanki <90123651+AtulSolanki@users.noreply.github.com>
2023-10-27 11:00:48 +0530

    Time: 8 ms (29.18%), Space: 9.3 MB (39.81%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -11,19 +11,26 @@ public:
     }
     int minimumTotal(vector<vector<int>>& a) {
         int n = a.size();
-        vector<vector<int>>dp(n,vector<int>(n,1e9));
+        vector<vector<int>>dp(n,vector<int>(n));
         //return solve(0,0,a,dp);
         dp[0][0] = a[0][0];
         if(n==1)return a[0][0];
         int ans = 1e9;
         for(int i=1;i<n;i++){   
             for(int j=0;j<=i;j++){
+                if(j==i){
+                    dp[i][j] = a[i][j] + dp[i-1][j-1];
+                    if(i==n-1)ans = min(ans,dp[i][j]);
+                    continue;
+                }
                 int c1 = a[i][j] + dp[i-1][j];
                 int c2 = 1e9;
                 if(j>0)c2 = a[i][j] + dp[i-1][j-1];
                 dp[i][j] = min(c1,c2);
+               // cout << dp[i][j] << " ";
                 if(i==n-1)ans = min(ans,dp[i][j]);
             }
+            //cout << endl;
         }
         return ans;
     }
