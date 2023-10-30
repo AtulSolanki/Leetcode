6bd49f6dbc6842de3c42a5fbb6379f868a740db9
AtulSolanki <90123651+AtulSolanki@users.noreply.github.com>
2023-10-30 17:26:29 +0530

    Time: 6 ms (76.61%), Space: 10.3 MB (81%) - LeetHub

--- 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.cpp
+++ 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-    vector<int> sortByBits(vector<int>& arr) {
-          vector<vector<int>>v(32);
-          int n = arr.size();
-          for(int i=0;i<n;i++){
-               int curr = arr[i] , cnt =0;
+    int bitCount (int curr){
+        int  cnt =0;
                 while(curr){
                       if(curr%2)cnt++;
                       curr /=2;
                 }
-                v[cnt].push_back(arr[i]);
-          }
-          int ind = 0 ;
-         for(int i=0;i<32;i++){
-             sort(v[i].begin(),v[i].end());
-             for(int j=0;j<v[i].size();j++){
-                 arr[ind++] = v[i][j];
-             }
-          }
-          return arr;
+            return cnt;
+    }
+    vector<int> sortByBits(vector<int>& arr) {
+            for(int i=0;i<arr.size();i++)
+        {
+            arr[i]+=bitCount(arr[i])*10001;
+        }
+        
+        sort(arr.begin(),arr.end());
+        
+        for(int i=0;i<arr.size();i++)
+        {
+            arr[i]=arr[i]%10001;
+        }
+        return arr;
     }
 };
