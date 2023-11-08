6b1a4cf0d8701251e189c70c46c328e873be54f9
AtulSolanki <90123651+AtulSolanki@users.noreply.github.com>
2023-11-08 18:48:20 +0530

    Time: 151 ms (15.27%), Space: 6.5 MB (95.08%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,28 +1,17 @@
 class Solution {
 public:
+    int helper(string& s, int i, int j) {
+        if (i >= j) return 1;
+        if(s[i]!=s[j])return 0;
+        return helper(s, i+1, j-1);
+    }
     int countSubstrings(string s) {
-          int n = s.size();
-          vector<vector<bool>> dp(n,vector<bool>(n,false));
-          int ans = 0;
-          for(int len=0;len<n;len++){
-              int i = 0;
-              for(int j=i+len;j<n;j++,i++){
-                  if(len==0) {
-                      dp[i][j] = 1;
-                  }
-                  else if(len==1){
-                      if(s[i]==s[j]) {
-                          dp[i][j] = 1;
-                      }
-                  }
-                  else{
-                      if(s[i]==s[j] && dp[i+1][j-1]){
-                          dp[i][j] = 1;
-                      }
-                  }
-                  ans += dp[i][j];
-              }
-          }
-          return ans;
+           int count = 0;
+        for(int i = 0; i < s.size(); ++i) {
+            for(int j = i; j < s.size(); ++j) {
+                count += helper(s, i, j);
+            }
+        }
+        return count;
     }
 };
